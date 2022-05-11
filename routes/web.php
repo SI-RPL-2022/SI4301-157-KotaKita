486a78a6ca991da436224a0743ff0422b8afe672
Mahardika Maulana <mahardikamaulana@student.telkomuniversity.ac.id>
2022-05-12 00:05:13 +0700

    Update Route ref #23

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProyekController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,20 +17,34 @@ use App\Http\Controllers\LoginController;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/registrasi', function () {
-    return view('registrasi');
-});
-Route::get('/faq', function () {
-    return view('Faq');
-});
-Route::get('/verif', function () {
-    return view('verifikasi');
-});
-Route::get('/kontak', function () {
-    return view('Contact');
-});
-Route::get('login', [LoginController::class, 'index']);
-Route::post('login', [LoginController::class, 'authenticate']);
+
+// Route::get('/faq', function () {
+//     return view('Faq');
+// });
+// Route::get('/verif', function () {
+//     return view('verifikasi');
+// });
+// Route::get('/kontak', function () {
+//     return view('Contact');
+// });
+// Route::get('/editprofile', function () {
+//     return view('editprofile1');
+// });
+// Route::get('/pilihproyek', function () {
+//     return view('pilihproyek');
+// });
+// Route::get('/proyek', function () {
+//     return view('proyek');
+// });
+// Route::get('/fase', function () {
+//     return view('fasebaru');
+// });
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/profile',[ProfileController::class,'index'])->name('profile.index');
+Route::get('/edit-profile',[ProfileController::class,'edit'])->name('profile.edit');
+
+// proyek
+Route::get('/proyek',[ProyekController::class,'index'])->name('proyek.index');
