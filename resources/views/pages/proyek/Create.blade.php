4a9bad2fb7ed21af84e021cc75baf1dcebdb1fa8
Mahardika Maulana <mahardikamaulana@student.telkomuniversity.ac.id>
2022-06-02 00:16:45 +0700

    update Halaman Tambah proyek dan Halaman proyek ref #10 #24

--- resources/views/pages/proyek/Create.blade.php
+++ resources/views/pages/proyek/Create.blade.php
@@ -3,48 +3,56 @@
 @section('content')
     <div class="container">
         <h1 class="my-5 fw-bold">BUAT PROYEK BARU</h1>
-        <form action="" method="post" enctype="multipart/form-data">
+        <form action="{{ route('proyek.store') }}" method="post" enctype="multipart/form-data">
             @csrf
             <div class="form-outline mb-2">
+                <label class="form-label" for="nama_proyek">Nama Proyek</label>
                 <input class="form-control form-control-lg" type="text" placeholder="Nama Proyek"
-                    aria-label=".form-control-lg example" required autofocus>
+                    aria-label=".form-control-lg example" required autofocus name="nama_proyek">
             </div>
             <div class="form-outline mb-2">
-                <label class="form-label" for="filenames[]">Tambah foto</label>
+                <label class="form-label" for="kota_id">Kota</label>
+                <select name="kota_id" id="kota_id" class="form-control">
+                    <option value="" selected disabled>-- Pilih Kota --</option>
+                    @foreach ($data_kota as $kota)
+                        <option value="{{ $kota->id }}">{{ $kota->nama }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-outline mb-2">
+                <label class="form-label" for="gambar">Tambah Gambar</label>
                 <div class="input-group">
-                    <input type="file" name="filenames[]" class="myfrm form-control">
-                    <div class="input-group-btn">
-                        <button class="btn btn-utama-inverted" type="button">Add</button>
-                    </div>
+                    <input type="file" name="gambar" class="myfrm form-control" id="gambar">
                 </div>
-                <p>*Maks 10mb</p>
-                {{-- Input File setelah Upload Berhasil --}}
-                <div>
-                    <div class=" input-group mt-3">
-                        <input type="file" name="filenames[]" class="myfrm form-control">
-                        <div class="input-group-btn">
-                            <button class="btn btn-danger" type="button">
-                                Remove</button>
-                        </div>
-                    </div>
+            </div>
+            <div class="form-outline mb-2">
+                <label class="form-label" for="logo">Tambah Logo</label>
+                <div class="input-group">
+                    <input type="file" id="logo" name="logo" class="myfrm form-control">
                 </div>
             </div>
             {{-- Input File Baru --}}
             <div class="row mb-2">
                 <div class="col">
                     <div class="form-outline">
-                        <label class="form-label" for="form6Example1">Tanggal Mulai</label>
-                        <input type="date" id="form6Example1" class="form-control" required />
+                        <label class="form-label" for="tanggal_mulai">Tanggal Mulai</label>
+                        <input type="date" id="tanggal_mulai" class="form-control" required  name="tanggal_mulai"/>
                     </div>
                 </div>
                 <div class="col">
                     <div class="form-outline">
-                        <label class="form-label" for="form6Example2">Tanggal Akhir</label>
-                        <input type="date" id="form6Example2" class="form-control" required />
+                        <label class="form-label" for="tanggal_akhir">Tanggal Akhir</label>
+                        <input type="date" id="tanggal_akhir" class="form-control" name="tanggal_akhir" />
                     </div>
                 </div>
             </div>
-            <div class="d-grid gap-2 mt-4 text-center"><button class="btn btn-utama" type="button">Kirim</button></div>
+            <div class="row">
+                <div class="form-outline">
+                    <label for="keterangan">Keterangan</label>
+                    <textarea name="keterangan" id="keterangan" cols="30" rows="4" class="form-control" placeholder="Keterangan"></textarea>
+                </div>
+            </div>
+            <div class="d-grid gap-2 mt-4 text-center"><button class="btn btn-utama" type="submit">Kirim</button></div>
         </form>
     </div>
 @endsection
--- resources/views/pages/proyek/show.blade.php
+++ resources/views/pages/proyek/show.blade.php
@@ -4,10 +4,10 @@
     <div class="container">
         <div class="row justify-content-between my-5">
             <div class="col-4">
-                <h1><b>NAMA PROYEK</b></h1>
+                <h1><b>{{ $item->nama_proyek }}</b></h1>
             </div>
             {{-- Rating --}}
-            <div class="col-4 d-grid gap-2 d-md-flex justify-content-md-end align-items-center">
+            {{-- <div class="col-4 d-grid gap-2 d-md-flex justify-content-md-end align-items-center">
                 <p class="align-items-center text-center align-middle my-auto fs-5"><b>5.0</b></p>
                 <div>
                     <span class="material-icons md-36">
@@ -26,20 +26,19 @@
                         star_rate
                     </span>
                 </div>
-            </div>
+            </div> --}}
             {{-- End Rating --}}
         </div>
         <div id="carouselExampleControls" class="carousel slide mb-5" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="{{ URL::to('/assets/img/Jumbotron.png') }}" class="d-block w-100" alt="...">
+                    <img src="{{ $item->gambar() }}" class="d-block w-100" alt="Foto Proyek">
                 </div>
+                @foreach ($galleries as $gallery)
                 <div class="carousel-item">
-                    <img src="{{ URL::to('/assets/img/Jumbotron.png') }}" class="d-block w-100" alt="...">
+                    <img src="{{ $gallery->foto() }}" class="d-block w-100" alt="Foto Proyek">
                 </div>
-                <div class="carousel-item">
-                    <img src="{{ URL::to('/assets/img/Jumbotron.png') }}" class="d-block w-100" alt="...">
-                </div>
+                @endforeach
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
                 data-bs-slide="prev">
@@ -60,7 +59,7 @@
                     Tanggal Mulai Proyek
                 </p>
                 <p class="text-center fs-5">
-                    01/01/2022
+                    {{ $item->tanggal_mulai->translatedFormat('d/m/Y') }}
                 </p>
             </div>
             <div class="col">
@@ -68,7 +67,11 @@
                     Update Terakhir Proyek
                 </p>
                 <p class="text-center fs-5">
-                    01/01/2022
+                    @if ($item->fases->count() > 0)
+                    {{ $item->firstFase()->tanggal_mulai->translatedFormat('d/m/Y') }}
+                    @else
+                    -
+                    @endif
                 </p>
             </div>
             <div class="col">
@@ -76,7 +79,11 @@
                     Estimasi Selesai Tahap saat ini
                 </p>
                 <p class="text-center fs-5">
-                    01/01/2022
+                    @if ($item->fases->count() > 0)
+                    {{ $item->firstFase()->tanggal_akhir->translatedFormat('d/m/Y') }}
+                    @else
+                    -
+                    @endif
                 </p>
             </div>
             <div class="col">
@@ -84,7 +91,7 @@
                     Estimasi Selesai Proyek
                 </p>
                 <p class="text-center fs-5">
-                    01/01/2022
+                    {{ $item->tanggal_akhir->translatedFormat('d/m/Y') }}
                 </p>
             </div>
 
@@ -92,37 +99,46 @@
         {{-- End Waktu --}}
         {{-- Button Input --}}
         {{-- Btn Admin --}}
-        {{-- <div class="row mb-5">
-            <div class="col">
-                <div class="d-grid">
-                    <a href="" class="btn btn-utama">Input Fase Baru</a>
+        @role('admin|super admin')
+            <div class="row mb-5">
+                <div class="col">
+                    <div class="d-grid">
+                        <a href="{{ route('fase.create', $item->id) }}" class="btn btn-utama">Input Fase Baru</a>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="d-grid">
+                        <a href="{{ route('fase-progress.create', $item->id) }}" class="btn btn-utama">Input Progress
+                            Baru</a>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="d-grid">
+                        <a href="{{ route('laporan.search') }}?q={{ $item->nama_proyek }}" class="btn btn-utama">Lihat Dokumen Laporan</a>
+                    </div>
                 </div>
             </div>
-            <div class="col">
-                <div class="d-grid">
-                    <a href="" class="btn btn-utama">Input Progress Baru</a>
-                </div>
-            </div>
-            <div class="col">
-                <div class="d-grid">
-                    <a href="" class="btn btn-utama">Lihat Dokumen Laporan</a>
-                </div>
-            </div>
-        </div> --}}
+        @endrole
         {{-- End Btn Admin --}}
         {{-- Btn User --}}
-        <div class="row mb-5">
-            <div class="col">
-                <div class="d-grid">
-                    <a href="" class="btn btn-utama">Input Aduan</a>
+        @role('user')
+            <div class="row mb-5">
+                @can('aduan-create')
+                <div class="col">
+                    <div class="d-grid">
+                        <a href="{{ route('aduan.create') }}?proyek_id={{ $item->id }}" class="btn btn-utama">Input Aduan</a>
+                    </div>
                 </div>
-            </div>
-            <div class="col">
-                <div class="d-grid">
-                    <a href="" class="btn btn-utama">Lihat Dokumen Laporan</a>
+                @endcan
+                @can('document-view')
+                <div class="col">
+                    <div class="d-grid">
+                        <a href="{{ route('laporan.search') }}?q={{ $item->nama_proyek }}" class="btn btn-utama">Lihat Dokumen Laporan</a>
+                    </div>
                 </div>
+                @endcan
             </div>
-        </div>
+        @endrole
         {{-- End Button Input --}}
         {{-- tb Fase --}}
         <div class="mb-5">
@@ -138,20 +154,23 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
+                    @forelse ($item->fases as $fase)
+                        <tr>
+                            <td>{{ $fase->nama_fase }}</td>
+                            <td>{!! $fase->status() !!}</td>
+                            <td>{{ $fase->tanggal_mulai->translatedFormat('d/m/Y') }}</td>
+                            <td>{{ $fase->tanggal_akhir->translatedFormat('d/m/Y') }}</td>
+                            <td>{{ $fase->updated_at->translatedFormat('d/m/Y') }}</td>
+                            <td>
+                                <button class="btn btn-sm btn-primary" data-fase="{{ $fase->id }}"
+                                    id="lihatProgress">Lihat Progress</button>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="5" class="text-center">Fase Tidak Ada</td>
+                        </tr>
+                    @endforelse
                 </tbody>
             </table>
         </div>
@@ -176,17 +195,8 @@
                         <th scope="col-2">Update Terakhir</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
+                <tbody class="progressTable">
+
                 </tbody>
             </table>
             {{-- End tb Progress --}}
@@ -194,34 +204,126 @@
         {{-- End Progress --}}
         {{-- Feedback --}}
         <div class="mb-5">
-            <h3 class="fw-bold mb-3">Feedback</h3>
+            <h3 class="fw-bold mb-3">Aduan</h3>
             {{-- Isi Feedback --}}
             <div class="border border-3 rounded">
-                <div class=" m-3">
-                    {{-- Rating --}}
-                    <div class="mb-3">
-                        <span class="material-icons md-36">
-                            star_rate
-                        </span>
-                        <span class="material-icons md-36">
-                            star_rate
-                        </span>
-                        <span class="material-icons md-36">
-                            star_rate
-                        </span>
-                        <span class="material-icons md-36">
-                            star_rate
-                        </span>
-                        <span class="material-icons md-36">
-                            star_rate
-                        </span>
+                @foreach ($item->aduans as $aduan)
+                    <div class=" m-3">
+                        {{-- Rating --}}
+                        <h5 class="fw-bold">{{ $aduan->user->name }}</h5>
+                        <p>{{ $aduan->aduan }}</p>
+                        <div class="row">
+                            @foreach ($aduan->foto as $foto)
+                                <div class="col-md-2">
+                                    <img src="{{ $foto->foto() }}" alt="" class="img-fluid imgAduan img-thumbnail"
+                                        data-img="{{ $foto->foto() }}">
+                                </div>
+                            @endforeach
+                        </div>
                     </div>
-                    <h5 class="fw-bold">Bambang</h5>
-                    <p>Hasilnya Sangat Memuaskan</p>
-                </div>
+                    <hr style="width:80%;text-align:center">
+                @endforeach
                 {{-- End Rating --}}
             </div>
         </div>
     </div>
     </div>
+    <div class="modal fade" id="modalShowImage" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Foto</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <img src="" alt="" class="img-fluid modalImage">
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
 @endsection
+@push('scripts')
+    <script type="text/javascript">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
+    <script>
+        $(function() {
+            var fase_id = '{{ $fase_id }}';
+            console.log(fase_id);
+            $.ajax({
+                url: '{{ route('fase-progress.getByFaseId') }}',
+                type: 'POST',
+                dataType: 'JSON',
+                data: {
+                    fase_id
+                },
+                success: function(response) {
+                    var xhtml = "";
+                    if (response.length > 0) {
+                        // $('.progressTable').html('');
+                        response.forEach(el => {
+                            xhtml += '<tr>';
+                            xhtml += '<td>' + el.judul_progress + '</td>';
+                            xhtml += '<td>' + el.deskripsi_singkat + '</td>';
+                            xhtml += '<td>' + el.updated_at + '</td>';
+                            xhtml += '</tr>';
+                        });
+                    } else {
+                        // $('.progressTable').html('');
+                        xhtml += '<tr>';
+                        xhtml +=
+                            '<td colspan="3" class="text-center">Tidak Ada Progress</td>';
+                        xhtml += '</tr>';
+                    }
+                    $('.progressTable').html(xhtml);
+                }
+            })
+
+            $('body').on('click', '#lihatProgress', function() {
+                var fase_id = $(this).data('fase');
+
+                $.ajax({
+                    url: '{{ route('fase-progress.getByFaseId') }}',
+                    type: 'POST',
+                    dataType: 'JSON',
+                    data: {
+                        fase_id
+                    },
+                    success: function(response) {
+                        var xhtml = "";
+                        if (response.length > 0) {
+                            // $('.progressTable').html('');
+                            response.forEach(el => {
+                                xhtml += '<tr>';
+                                xhtml += '<td>' + el.judul_progress + '</td>';
+                                xhtml += '<td>' + el.deskripsi_singkat + '</td>';
+                                xhtml += '<td>' + el.updated_at + '</td>';
+                                xhtml += '</tr>';
+                            });
+                        } else {
+                            // $('.progressTable').html('');
+                            xhtml += '<tr>';
+                            xhtml +=
+                                '<td colspan="3" class="text-center">Tidak Ada Progress</td>';
+                            xhtml += '</tr>';
+                        }
+                        $('.progressTable').html(xhtml);
+                    }
+                })
+            })
+            $('body').on('click', '.imgAduan', function(){
+                var url = $(this).data('img');
+                $('.modalImage').attr('src',url);
+                $('#modalShowImage').modal('show');
+            })
+
+        })
+    </script>
+@endpush
