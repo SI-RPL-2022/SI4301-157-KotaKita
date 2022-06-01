4edac8fa4cd91543f3614eaca57123f523d6d3b4
Mahardika Maulana <mahardikamaulana@student.telkomuniversity.ac.id>
2022-06-02 00:27:26 +0700

    update halaman tambah fase dan tambah progress ref #30 #12

--- resources/views/pages/Progress/create.blade.php
+++ resources/views/pages/Progress/create.blade.php
@@ -3,20 +3,29 @@
 @section('content')
     <div class="container">
         <h1 class="my-5 fw-bold">TAMBAH PROGRESS PROYEK</h1>
-        <form action="" method="post" enctype="multipart/form-data">
+        <form action="{{ route('fase-progress.store') }}" method="post" enctype="multipart/form-data">
             @csrf
+            {{-- Get dari Fase Terakhir --}}
             <div class="form-outline mb-2">
-                <label class="form-label fw-bold" for="form6Example5">Judul Progress</label>
-                <input type="email" id="form6Example5" class="form-control" required />
+                <label class="form-label fw-bold" for="fase_id">Fase</label>
+                <input type="text" id="fase_id" name="fase_id" class="form-control" hidden value="{{ $fase->id }}" />
             </div>
             <div class="form-outline mb-2">
-                <label class="form-label fw-bold" for="form6Example5">Deskripsi Singkat Progress</label>
-                <input type="email" id="form6Example5" class="form-control" required />
+                <label class="form-label fw-bold" for="judul_progress">Judul Progress</label>
+                <input type="text" id="judul_progress" name="judul_progress" class="form-control" required />
             </div>
-            {{-- Input File Baru --}}
-            <label class="form-label fw-bold" for="form6Example5">Input Laporan</label>
+            <div class="form-outline mb-2">
+                <label class="form-label fw-bold" for="deskripsi_singkat">Deskripsi Singkat Progress</label>
+                <textarea name="deskripsi_singkat" id="deskripsi_singkat" cols="30" rows="3" class="form-control"></textarea>
+            </div>
+            <div class="form-outline mb-2">
+                <label class="form-label fw-bold" for="pengeluaran">Pengeluaran</label>
+                <input type="text" id="pengeluaran" name="pengeluaran" class="form-control" required />
+            </div>
+            {{-- Input File Baru, untuk input laporan ini cuma bisa ekstensi pdf aja --}}
+            <label class="form-label fw-bold" for="document">Input Laporan</label>
             <div class="input-group">
-                <input type="file" name="filenames[]" class="myfrm form-control">
+                <input type="file" name="document[]" class="myfrm form-control" multiple>
                 <div class="input-group-btn">
                     <button class="btn btn-utama-inverted" type="button"><i
                             class="fldemo glyphicon glyphicon-plus"></i>Add</button>
@@ -24,7 +33,7 @@
             </div>
             <p>*Maks 10mb</p>
             {{-- Input File setelah Upload Berhasil --}}
-            <div>
+            {{-- <div>
                 <div class=" input-group mt-3">
                     <input type="file" name="filenames[]" class="myfrm form-control">
                     <div class="input-group-btn">
@@ -32,11 +41,11 @@
                             Remove</button>
                     </div>
                 </div>
-            </div>
-            {{-- Input File Baru --}}
-            <label class="form-label mt-3 fw-bold" for="form6Example5">Input Foto</label>
+            </div> --}}
+            {{-- Input File Baru, untuk bagian foto cuma bisa foto aja --}}
+            <label class="form-label mt-3 fw-bold" for="foto">Input Foto</label>
             <div class="input-group">
-                <input type="file" name="filenames[]" class="myfrm form-control">
+                <input type="file" name="foto[]" id="foto" class="myfrm form-control" multiple>
                 <div class="input-group-btn">
                     <button class="btn btn-utama-inverted" type="button"><i
                             class="fldemo glyphicon glyphicon-plus"></i>Add</button>
@@ -44,7 +53,7 @@
             </div>
             <p>*Maks 10mb</p>
             {{-- Input File setelah Upload Berhasil --}}
-            <div>
+            {{-- <div>
                 <div class=" input-group mt-3">
                     <input type="file" name="filenames[]" class="myfrm form-control">
                     <div class="input-group-btn">
@@ -52,16 +61,16 @@
                             Remove</button>
                     </div>
                 </div>
-            </div>
+            </div> --}}
 
             <div class="form-check mt-3">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                <label class="form-check-label" for="flexCheckDefault">
-                  Selesaikan Fase saat ini?
+                <input class="form-check-input" type="checkbox" name="status" id="status">
+                <label class="form-check-label" for="status">
+                    Selesaikan Fase saat ini?
                 </label>
-              </div>
+            </div>
 
-            <div class="d-grid gap-2 mt-4 text-center"><button class="btn btn-utama" type="button">Kirim</button></div>
+            <div class="d-grid gap-2 mt-4 text-center"><button class="btn btn-utama" type="submit">Kirim</button></div>
         </form>
         </form>
     </div>
--- resources/views/pages/fase/create.blade.php
+++ resources/views/pages/fase/create.blade.php
@@ -2,69 +2,65 @@
 @section('title', '- Buat Fase Baru')
 @section('content')
 
-<div class="container">
-    <h1 class="fw-bold my-5">BUAT FASE BARU</h1>
-    {{-- form buat fase --}}
-    <form action="" method="post" enctype="multipart/form-data">
-    @csrf
-        <div class="form-outline mb-2">
-            <label class="form-label" for="form6Example2">Pilih Fase</label>
-            <select class="form-select" aria-label="Default select example">
-                <option selected>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-        </div>
-        <div class="row mb-2">
-            <div class="col">
-                <div class="form-outline">
-                    <label class="form-label" for="form6Example1">Nama Awal</label>
-                    <input type="text" id="form6Example1" class="form-control" />
+    <div class="container">
+        <h1 class="fw-bold my-5">BUAT FASE BARU</h1>
+        {{-- form buat fase --}}
+        <form action="{{ route('fase.store') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            {{-- Get dari Proyek --}}
+            <div class="form-outline mb-2" hidden>
+                <label class="form-label fw-bold" for="proyek_id">Proyek</label>
+                <input type="number" id="proyek_id" class="form-control" required name="proyek_id" value="{{ $proyek_id }}"/>
+            </div>
+            <div class="form-outline mb-2">
+                <label class="form-label" for="nama_fase">Nama Fase</label>
+                <input type="text" id="nama_fase" class="form-control" name="nama_fase" value="{{ old('nama_fase') }}"/>
+            </div>
+            <div class="row mb-2">
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="tanggal_mulai">Tanggal Mulai</label>
+                        <input type="date" id="tanggal_mulai" class="form-control" name="tanggal_mulai" value="{{ old('tanggal_mulai') }}"/>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="tanggal_akhir">Tanggal Akhir</label>
+                        <input type="date" id="tanggal_akhir" class="form-control" name="tanggal_akhir" value="{{ old('tanggal_akhir') }}"/>
+                    </div>
                 </div>
             </div>
-            <div class="col">
-                <div class="form-outline">
-                    <label class="form-label" for="form6Example2">Nama Akhir</label>
-                    <input type="text" id="form6Example2" class="form-control" />
+
+            <div class="form-outline mb-2">
+                <label class="form-label" for="estimasi_pendanaan">Estimasi Pendanaan</label>
+                <input type="number" id="estimasi_pendanaan" class="form-control" name="estimasi_pendanaan" value="{{ old('estimasi_pendanaan') }}"/>
+            </div>
+            {{-- ini untuk input awal --}}
+            <div class="input-group mb-3">
+                <input type="file" class="form-control" name="file[]" id="file" multiple>
+                <label class="input-group-text" for="file">Upload</label>
+            </div>
+            {{-- <div class="input-group">
+                <input type="file" name="filenames[]" class="myfrm form-control">
+                <div class="input-group-btn">
+                    <button class="btn btn-utama-inverted" type="button"><i
+                            class="fldemo glyphicon glyphicon-plus"></i>Add</button>
                 </div>
+            </div> --}}
+            {{-- ini untuk input yg udah ada, jadi buat edit atau delete --}}
+            {{-- <div class="clone hide">
+                <div class="input-group" style="margin-top:10px">
+                    <input type="file" name="filenames[]" class="myfrm form-control">
+                    <div class="input-group-btn">
+                        <button class="btn btn-danger" type="button"><i class="fldemo glyphicon glyphicon-remove"></i>
+                            Remove</button>
+                    </div>
+                </div>
+            </div> --}}
+            <div class="d-grid gap-2 mt-4 text-center">
+                <button class="btn btn-utama" type="submit">Kirim</button>
             </div>
-        </div>
-        <div class="form-outline mb-2">
-            <label class="form-label" for="form6Example5">Email</label>
-            <input type="email" id="form6Example5" class="form-control" />
-        </div>
-        <div class="form-outline mb-2">
-            <label class="form-label" for="form6Example4">Domisili</label>
-            <input type="text" id="form6Example4" class="form-control" />
-        </div>
-
-        <div class="form-outline mb-2">
-            <label class="form-label" for="form6Example6">Telepon</label>
-            <input type="number" id="form6Example6" class="form-control" />
-        </div>
-
-        <div class="input-group mb-3">
-            <input type="file" class="form-control" id="inputGroupFile02">
-            <label class="input-group-text" for="inputGroupFile02">Upload</label>
-          </div>
-
-          <div class="input-group hdtuto control-group lst increment" >
-            <input type="file" name="filenames[]" class="myfrm form-control">
-            <div class="input-group-btn">
-              <button class="btn btn-utama-inverted" type="button"><i class="fldemo glyphicon glyphicon-plus"></i>Add</button>
-            </div>
-          </div>
-          <div class="clone hide">
-            <div class="hdtuto control-group lst input-group" style="margin-top:10px">
-              <input type="file" name="filenames[]" class="myfrm form-control">
-              <div class="input-group-btn">
-                <button class="btn btn-danger" type="button"><i class="fldemo glyphicon glyphicon-remove"></i> Remove</button>
-              </div>
-            </div>
-          </div>
-        <div class="d-grid gap-2 mt-4 text-center"><button class="btn btn-utama" type="button">Kirim</button></div>
-    </form>
-    {{-- end form --}}
-</div>
+        </form>
+        {{-- end form --}}
+    </div>
 @endsection
