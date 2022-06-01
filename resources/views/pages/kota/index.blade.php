083d536ef65ecece8be1f9c4d4b4569f9f49652d
mhmdfathh <mfathirabbani20@gmail.com>
2022-06-01 23:25:56 +0700

    Update halaman pilih kota ref #18

--- resources/views/pages/kota/index.blade.php
+++ resources/views/pages/kota/index.blade.php
@@ -8,18 +8,20 @@
             </div>
             {{-- Button Buat Proyek --}}
             <div class="col-2 d-grid gap-2 d-md-flex justify-content-md-end align-items-center">
-              <a href="" class="btn btn-utama">Buat Kota Baru</a>
+              <a href="{{ route('kota.create') }}" class="btn btn-utama">Buat Kota Baru</a>
             </div>
           </div>
 
         {{-- Card Grid --}}
         <div class="row row-cols-1 row-cols-md-4 g-4">
             {{-- card --}}
+            @foreach ($data_kota as $kota)
             <div class="col">
                 <div class="card h-100 border-white">
-                    <img src="..." class="card-img-top" alt="...">
+                    {{-- imagenya diisi sama foto paling awal --}}
+                    <img src="{{ $kota->gambar() }}" class="card-img-top" alt="Foto Kota">
                     <div class="card-body">
-                        <h5 class="card-title text-center">Card title</h5>
+                        <h5 class="card-title text-center">{{ $kota->nama }}</h5>
                         <div class="row gx-3">
                             <div class="col">
                                 <div class="d-grid">
@@ -28,73 +30,14 @@
                             </div>
                             <div class="col">
                                 <div class="d-grid">
-                                    <a href="" class="btn btn-utama">Edit</a>
+                                    <a href="{{ route('kota.edit',$kota->id) }}" class="btn btn-utama">Edit</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            {{-- End Card --}}
-            {{-- Card Cek --}}
-            <div class="col">
-                <div class="card h-100 border-white">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Card title</h5>
-                        <div class="row gx-3">
-                            <div class="col">
-                                <div class="d-grid">
-                                    <a href="" class="btn btn-utama">Cek</a>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {{-- End Card Cek --}}
-            <div class="col">
-                <div class="card h-100 border-white">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Card title</h5>
-                        <div class="row gx-3">
-                            <div class="col">
-                                <div class="d-grid">
-                                    <a href="" class="btn btn-utama">Cek</a>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="d-grid">
-                                    <a href="" class="btn btn-utama">Edit</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100 border-white">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Card title</h5>
-                        <div class="row gx-3">
-                            <div class="col">
-                                <div class="d-grid">
-                                    <a href="" class="btn btn-utama">Cek</a>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="d-grid">
-                                    <a href="" class="btn btn-utama">Edit</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 @endsection
-
